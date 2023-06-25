e9ffa28930118964375706aa5b63843b1f637138
adriano-itspay <adriano.andrade@itspay.com.br>
2023-06-25 02:18:17 -0300

    Atualizações no design e novos componentes

--- js/script.js
+++ js/script.js
@@ -13,18 +13,28 @@ const sendWpp = (e) => {
 
     let nome = document.getElementById('nome').value
     let telefone = document.getElementById('telefone').value
-    let email = document.getElementById('email').value
-    let tipoAtendimento = document.getElementById('tipoAtendimento').value
-    let tipoPaciente = document.getElementById('tipoPaciente').value
     let mensagem = document.getElementById('mensagem').value
 
     total = 
     `Nome: ${nome}%0a` +
     `Telefone: ${telefone}%0a` +
-    `E-mail: ${email}%0a` +
-    `Tipo de atendimento: ${tipoAtendimento}%0a` +
-    `Tipo de paciente: ${tipoPaciente}%0a` +
     `Mensagem: ${mensagem}`
 
     window.open(`https:web.whatsapp.com/send?phone=5561983733961&text=${total}`,"_blank")
 }
+
+const initMap = () => {
+    const uluru = { lat: -25.344, lng: 131.031 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+window.initMap = initMap;
